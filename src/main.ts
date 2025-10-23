bcf3b94c9284b32eaa0fcfd1d35f9c123535101c
cryptomagiciian <your-email@example.com>
2025-10-23 02:39:27 -0700

    Fix compression middleware import issue
    - Change compression import from 'import compression from' to 'import * as compression from'
    - This fixes the 'compression_1.default is not a function' error
    - The compression package uses CommonJS exports which need namespace import

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe, Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
-import compression from 'compression';
+import * as compression from 'compression';
 import { AppModule } from './app.module';
 import { setupTracing } from './common/tracing';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
